06d71e6f01ac35ddf6d29aa6d1ea4458ea519939
Roxanne Nasraty <rsnasraty@gmail.com>
2020-11-11 10:30:15 -0600

    basic css

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
 function onDragStart(event) {
-    event 
-    .dataTransfer
-    .setData('text/plain', event.target.id);
+  event.dataTransfer.setData("text/plain", event.target.id);
 
-    event
-    .currentTarget
-    .style
-    .backgroundColor = 'yellow';
+  event.currentTarget.style.backgroundColor = "pink";
 }
 
 function onDragOver(event) {
-    event.preventDefault();
+  event.preventDefault();
 }
 
 function onDrop(event) {
-    const id = event
-    .dataTransfer
-    .getData('text');
+  const id = event.dataTransfer.getData("text");
 
-    const draggableElement = document.getElementById(id);
+  const draggableElement = document.getElementById(id);
 
-    const dropzone = event.target;
+  const dropzone = event.target;
 
-    dropzone.appendChild(draggableElement);
+  dropzone.appendChild(draggableElement);
 
-    event
-    .dataTransfer
-    .clearData();
-
-
+  event.dataTransfer.clearData();
 }
